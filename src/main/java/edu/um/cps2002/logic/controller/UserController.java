cdbebb6d5f6da863a41758047d4f702a7f32e5d9
Álvaro <avoluke05@gmail.com>
2026-05-27 12:27:23 +0200

    Refined methods to modify pass the CI test

--- src/main/java/edu/um/cps2002/logic/controller/UserController.java
+++ src/main/java/edu/um/cps2002/logic/controller/UserController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 //To execute the page of user enter into http://localhost:8080/user/screenings
 @RestController
-@RequestMapping("/user") // Changed from /api to /user
+@RequestMapping("/api") // Changed from /api to /user
 @CrossOrigin(origins = "*") // Allows VS Code to talk to your Java server
 public class UserController {
     private BookingService bookingService = new BookingService();
@@ -29,28 +29,13 @@ public class UserController {
         }
 
         try {
-            bookingService.createBooking("User", id, seats);
+            bookingService.createBooking("User", id,seats);
 
             return ResponseEntity.ok(screening);
 
         } catch (IllegalArgumentException e) {
             // 5. Catch the error and return the string message instead
             return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @GetMapping("/select-screen")
-    public ResponseEntity<?> selectScreen(@RequestParam String screeningId) {
-        try {
-            Screening screening = CinemaDatabase.getInstance().findScreeningById(screeningId);
-            
-            if (screening == null) {
-                return ResponseEntity.ok("Error: Screening ID not found.");
-            }
-            
-            return ResponseEntity.ok("You have selected screening: " + screening.getMovieTitle());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Internal Server Error: " + e.getMessage());
         }
     }
 }
